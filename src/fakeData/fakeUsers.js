223208c70e3a85694c7f3d25f4c560966ee16ef5
Pathomratan <Pathomrat.an@hotmail.com>
2026-05-13 14:51:54 +0700

    Setup Express server

---
+++ src/fakeData/fakeUsers.js
@@ -0,0 +1,19 @@
+export const users = [
+    {
+        id:"1", 
+        usename: "Neeti", 
+        email: "neeti@example.com", 
+        password: "pass123"
+    },
+    {
+        id:"2", 
+        usename: "Alice", 
+        email: "Alice@example.com", 
+        password: "pass456"
+    },
+    {
+        id:"3", 
+        usename: "Mane", 
+        email: "Mane@example.com", 
+        password: "pass789"
+    }]
